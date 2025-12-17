9e5ccf8497fe3af3878de5944c3d55ccc96c5aa5
Jessi Cruz Betancourt <cruzbetancourtjessi@gmail.com>
2025-12-17 14:37:28 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8783231251-fea41",
+  "appId": "1:436069048916:web:be1a9d39f7992ab23843c1",
+  "apiKey": "AIzaSyDX2yBXeI0TZ4PgyVraNGAYppx-uwO1mEU",
+  "authDomain": "studio-8783231251-fea41.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "436069048916"
+};
