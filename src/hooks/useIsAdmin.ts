d76992b71b35eddc5175b8c32de14ebe93933017
Jessi Cruz Betancourt <cruzbetancourtjessi@gmail.com>
2025-12-18 05:42:40 +0000

    se me desaparecio las opciones de administrador

--- src/hooks/useIsAdmin.ts
+++ src/hooks/useIsAdmin.ts
@@ -4,11 +4,11 @@ import { useUser } from '@/firebase';
 
 // --- ¡IMPORTANTE! ---
 // Este es el correo electrónico que se considerará como administrador.
-// Cámbialo por el correo que usarás para administrar la aplicación.
+// ¡Cámbialo por el tuyo para asegurar el panel de administración!
 const ADMIN_EMAIL = 'admin@example.com'; 
 
 export function useIsAdmin() {
     const { user } = useUser();
-    // La lógica ahora comprueba si el email del usuario coincide con el del administrador.
+    // La lógica comprueba si el email del usuario conectado coincide con el del administrador.
     return user?.email === ADMIN_EMAIL;
 }
