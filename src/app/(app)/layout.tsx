4cd663afa6f1191702d68a8488b2881128f24bdc
Jessi Cruz Betancourt <cruzbetancourtjessi@gmail.com>
2025-12-20 00:09:43 +0000

    Try fixing this error: `Build Error: x Expected '</', got 'jsx text (. E

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -135,7 +135,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
                     <ShieldCheck />
                     Admin
                   </Link>
-                </SidebarMenuBodieuton>
+                </SidebarMenuButton>
               </SidebarMenuItem>
             )}
             <SidebarMenuItem>
