6b39c687d728d847358200b06658a0e51d01864c
Jessi Cruz Betancourt <cruzbetancourtjessi@gmail.com>
2026-01-28 17:48:43 +0000

    no abre aun

--- src/hooks/useIsAdmin.ts
+++ src/hooks/useIsAdmin.ts
@@ -3,12 +3,12 @@
 import { useUser } from '@/firebase';
 
 // --- ¡IMPORTANTE! ---
-// Este es el CORREO del usuario que se considerará como administrador.
-// Para obtener privilegios de administrador, crea o utiliza una cuenta con este correo exacto.
-const ADMIN_EMAIL = 'rosembercruzbetancourt@gmail.com'; 
+// Este es el UID del usuario que se considerará como administrador.
+// Para obtener privilegios de administrador, debes iniciar sesión con la cuenta correspondiente.
+const ADMIN_UID = 'LMXNKsZh9NMgk3tKoyyqYlqcbFY2'; 
 
 export function useIsAdmin() {
     const { user } = useUser();
-    // La lógica comprueba si el email del usuario conectado coincide con el del administrador.
-    return user?.email === ADMIN_EMAIL;
+    // La lógica comprueba si el UID del usuario conectado coincide con el del administrador.
+    return user?.uid === ADMIN_UID;
 }
